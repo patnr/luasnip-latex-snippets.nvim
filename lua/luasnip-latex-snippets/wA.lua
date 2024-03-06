0b3fd86e53724e977948e4fb252928cac54e4cc5
patnr <patrick.n.raanes@gmail.com>
2024-03-06 16:04:27 +0100

    Yay!

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -34,8 +34,8 @@ function M.retrieve(not_math)
     parse_snippet({ trig = "single_quote_snip", name = "Single quotes" }, "\\enquote*{${1:${TM_SELECTED_TEXT}}}${0}"),
 
     parse_snippet({ trig = "left_right(", name = "Cancel left_right" }, "(($0))"),
-    parse_snippet({ trig = "left_right[", name = "Cancel left_right" }, "[[$0]]"),
     parse_snippet({ trig = "left_right{", name = "Cancel left_right" }, "{{$0}}"),
+    parse_snippet({ trig = "left_right[", name = "" }, "\\[ $0 \\]"),
 
     parse_snippet({ trig = "(_2snip", name = "Autopair paranthesis with jumpable placeholder" }, "(${1})$0"),
     parse_snippet({ trig = "[_2snip", name = "Autopair bracket with jumpable placeholder" }, "[${1}]$0"),
