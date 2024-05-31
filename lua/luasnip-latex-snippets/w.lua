c7005a4a944b5eb01b4ffbabcfbffa6923441663
patnr <patrick.n.raanes@gmail.com>
2024-05-31 13:31:59 +0200

    Add MWE

--- lua/luasnip-latex-snippets/w.lua
+++ lua/luasnip-latex-snippets/w.lua
@@ -48,6 +48,114 @@ function M.retrieve(is_math, not_math)
     parse_snippet({ trig = "quote_builtin_block", name = "Block quote (builtin)" },
       "\\begin{quotation}\n\t$1\n\\end{quotation}$0"),
 
+
+    s(
+      {
+        trig = "MWE",
+        dscr = "Minimal working example",
+      },
+      fmta([[
+        \documentclass[10pt,a4paper]{article}
+        \usepackage{parskip}
+        % \linespread{1.5}
+        \usepackage[top=2.0cm,bottom=1.9cm,left=1.5cm,right=1.5cm]{geometry}
+        % \usepackage[authoryear,round,elide,nonamebreak]{natbib}
+        % \usepackage[sort,numbers]{natbib}
+        \usepackage[utf8]{inputenc}
+        \usepackage[french,english]{babel}
+        \usepackage[T1]{fontenc}
+        \usepackage{lmodern}
+        \usepackage[svgnames]{xcolor}
+
+        \usepackage{amsfonts,amssymb}    % define ensembles
+        \usepackage[intlimits]{amsmath}  % intlimits for limits above/below
+        \usepackage[]{mathalfa}
+        \usepackage{mathrsfs}            % For laplace transform \mathscr{L}
+        \usepackage{upgreek}             % Big greek letters
+        \usepackage{dsfont}              % Double stroke fonts
+        \usepackage{mathtools}           % Delimiter (such as norm) stuff
+        \usepackage{relsize}             % Resize maths symbols
+        \usepackage{cancel}              % Crossing out stuff
+        \usepackage{accents}             % Adds more accents.
+        \usepackage{bm}                  % For boldface greek symbols
+
+        \usepackage{float}
+        \usepackage[small,bf]{caption}
+        \usepackage{mdframed} % Fancy frames
+        \usepackage{csquotes}
+
+        \definecolor{mycc}{rgb}{0, 0, 0.45}
+        \definecolor{mygreen}{rgb}{0, 0.4, 0}
+
+        \usepackage[pdftex,
+          pdftitle={},
+          pdfauthor={},
+          hyperindex=true,
+          colorlinks=true,
+          linkcolor=mygreen,
+          citecolor=mycc,
+              urlcolor=blue,
+              ]{hyperref}
+        \usepackage[pdftex]{graphicx}
+        \usepackage{epstopdf}
+        \usepackage[stretch=10]{microtype}
+        \usepackage{hyperref}
+        \usepackage[doipre={DOI:~}]{uri} % loads "url", if not already loaded
+
+        \graphicspath{{./}{Pics/}}
+        \urlstyle{tt}
+
+        \usepackage{cleveref}
+
+        \newcommand{\nospell}[1]{#1}
+
+        \newcommand{\mat}[1]{{\mathbf{{#1}}}}
+        % \newcommand{\mat}[1]{{\pmb{\mathsf{#1}}}}
+        \newcommand{\vect}[1]{{\bm{#1}}}
+        \newcommand{\x}{\vect{x}}
+        \newcommand{\y}{\vect{y}}
+        \newcommand{\z}{\vect{z}}
+
+        \newcommand*{\myfun}[1]{\mathinner{#1}}
+        \newcommand{\ones}[0]{\mathds{1}}
+
+        \newcommand{\TP}{{\mathsf{T}}}
+        \newcommand{\tr}{\ensuremath{^{\TP}}}
+        \newcommand{\pinvsign}{{+}}
+        \newcommand{\pinv}{\ensuremath{^\pinvsign}}
+        \newcommand{\pinvtr}{\ensuremath{^{\pinvsign \TP}}}
+
+        \begin{document}
+        \title{
+            \vspace{-1cm}
+            {
+                \vspace{-1em}
+            Minimal working example
+                \vspace{-1em}
+            }
+        }
+        \author{Patrick N. Raanes}
+        \date{\vspace{-1em}\today}
+        \maketitle
+        % \begin{abstract}
+        % \end{abstract}
+        % \vspace{1em}
+
+        Hello world!
+        <>
+
+        %\newpage
+        % \bibliographystyle{plainnat}
+        % \bibliographystyle{unsrtnat}
+        % \bibliographystyle{myabbrvnat}
+        % \bibliography{references}
+        \end{document}
+      ]], {
+          i(1),
+        }
+      )
+    ),
+
   }
 end
 
