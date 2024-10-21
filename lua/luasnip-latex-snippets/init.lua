6e68af83b0184a9285a4124f9fe4ae994608acb4
patnr <patrick.n.raanes@gmail.com>
2024-10-21 16:58:46 +0200

    Fix for markdown w/o treesitter

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -106,8 +106,9 @@ end
 M.setup_markdown = function()
   local ls = require("luasnip")
 
-  local is_math = utils.with_opts(utils.is_math, true)
-  local not_math = utils.with_opts(utils.not_math, true)
+  local treesitter = vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] ~= nil
+  local is_math = utils.with_opts(utils.is_math, treesitter)
+  local not_math = utils.with_opts(utils.not_math, treesitter)
 
   local autosnippets = {}
   for _, s in ipairs({
