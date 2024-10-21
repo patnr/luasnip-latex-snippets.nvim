eb305683ef131c806fe915f1899595451ada8e39
patnr <patrick.n.raanes@gmail.com>
2024-10-21 17:04:03 +0200

    Markdown should just use all math snippets

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -109,39 +109,22 @@ M.setup_markdown = function()
   local is_math = utils.with_opts(utils.is_math, true)
   local not_math = utils.with_opts(utils.not_math, true)
 
-  local math_i = require("luasnip-latex-snippets/math_i").retrieve(is_math)
-  ls.add_snippets("markdown", math_i, { default_priority = 0 })
-
-  local autosnippets = _autosnippets(is_math, not_math)
-  local trigger_of_snip = function(s)
-    return s.trigger
-  end
-
-  local to_filter = {}
-  for _, str in ipairs({
-    "wA",
-    "bwA",
+  local autosnippets = {}
+  for _, s in ipairs({
+    "math_wRA_no_backslash",
+    "math_rA_no_backslash",
+    "math_wA_no_backslash",
+    "math_iA_no_backslash",
+    "math_iA",
+    "math_wrA",
   }) do
-    local t = require(("luasnip-latex-snippets.%s"):format(str)).retrieve(not_math)
-    vim.list_extend(to_filter, vim.tbl_map(trigger_of_snip, t))
+    vim.list_extend(
+      autosnippets,
+      require(("luasnip-latex-snippets.%s"):format(s)).retrieve(is_math)
+    )
   end
 
-  local filtered = vim.tbl_filter(function(s)
-    return not vim.tbl_contains(to_filter, s.trigger)
-  end, autosnippets)
-
-  local parse_snippet = ls.extend_decorator.apply(ls.parser.parse_snippet, {
-    condition = pipe({ not_math }),
-  }) --[[@as function]]
-
-  -- tex delimiters
-  local normal_wA_tex = {
-    parse_snippet({ trig = "mk", name = "Math" }, "$${1:${TM_SELECTED_TEXT}}$"),
-    parse_snippet({ trig = "dm", name = "Block Math" }, "$$\n\t${1:${TM_SELECTED_TEXT}}\n.$$"),
-  }
-  vim.list_extend(filtered, normal_wA_tex)
-
-  ls.add_snippets("markdown", filtered, {
+  ls.add_snippets("markdown", autosnippets, {
     type = "autosnippets",
     default_priority = 0,
   })
